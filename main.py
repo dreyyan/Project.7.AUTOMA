e5ee83a6163659ceee60f4c21ba7424e4e9eb6dd
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-07-22 23:06:04 +0800

    working on app open and close

--- main.py
+++ main.py
@@ -15,42 +15,42 @@ import spacy
 import speech_recognition as sr
 from halo import Halo
 import webbrowser
+import pygame
 
 import time, json
 import sys, os, subprocess
 
 ''' UTILITIES '''
-# UTILITY: load model & entities
-def prepare_data():
-    entities = load_entities() # load entities
-    
-    browsers_dict = entities.get("browsers", {})
-    browsers_dict = {browser.lower(): command for browser, command in browsers_dict.items()}
-
-    return entities, browsers_dict
-
-# UTILITY: load 'entities.json'
+# UTILITY: Load 'entities.json'
 def load_entities():
     with open("entities.json", 'r', encoding="utf-8") as file:
         return json.load(file)
     
-# UTILITY: display bot response
+# UTILITY: Display Bot's Response
 def response(bot_response) -> None:
+    pygame.mixer.init()
+    pygame.mixer.music.load("assets/type_SFX.mp3")
+    pygame.mixer.music.set_volume(0.1)
+
     character_delay_animation(f"[AUTOMA]: ", 0.03, 0)
-    delay(1.5)
+    delay(1)
+
+    pygame.mixer.music.play()
     character_delay_animation(f"{bot_response}", 0.03, 1)
+
+    pygame.mixer.music.fadeout(500)
     
-# UTILITY: display header
+# UTILITY: Display Header
 def display_header():
     character_delay_animation(f"     `~`~`~ [A].[U].[T].[O].[M].[A]. ~`~`~`", 0.03, 1)
-    character_delay_animation(f"{'#' * 48}", 0.03, 1)
-    
+    character_delay_animation(f"{'#' * 48}", 0.02, 1)
+
 ''' PROCESS '''
-# PROCESS: welcome user
+# PROCESS: Welcomes the User
 def greet():
     response("Hi, I'm A.U.T.O.M.A. Ask me anything!")
 
-# PROCESS: get user prompt via voice recognition
+# PROCESS: Get User Prompt via Voice Recognition
 def listen_for_response():
     # create an instance of 'Recognizer'
     recognizer = sr.Recognizer()
@@ -82,63 +82,143 @@ def listen_for_response():
 
     return translated_text
 
-# PROCESS: use NLP english model to create doc object
-def process_prompt(user_prompt): 
+# PROCESS: Use NLP English Model to Create 'doc' Object
+def process_prompt(user_prompt):
+    pygame.mixer.init()
+    pygame.mixer.music.load("assets/type_SFX.mp3")
+    pygame.mixer.music.set_volume(0.1)
+
     nlp = spacy.load("en_core_web_sm") # load spacy NLP english model
-    character_delay_animation(f"[You]: ", 0.03, 0)
+    character_delay_animation(f"   [You]: ", 0.03, 0)
+    delay(1)
+
+    pygame.mixer.music.play()
     character_delay_animation(f"{user_prompt}", 0.03, 1)
+    pygame.mixer.music.fadeout(500)
+
     doc = nlp(user_prompt)
     return doc
 
-# PROCESS: register browsers for recognition
+# PROCESS: Register Specified Browser
 def register_browser():
     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
-    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))   
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
-# PROCESS: open the specified browser
-def open_browser(entities, doc, browsers_dict): 
-    lowercase_text = doc.text.lower()
+# PROCESS: Return Command Name of Specified Browser
+def get_specified_browser(lowercase_text, browsers_dict):
     for browser_friendly_lower, command_name in browsers_dict.items():
         if browser_friendly_lower in lowercase_text:
-            specified_browser = command_name
+            return command_name
+    return None
 
-            try:
-                register_browser()
-                response(f"I'm opening {specified_browser} for you...")
-                
-                browser = webbrowser.get(specified_browser)
-                browser.open('https://www.google.com')
-                break
+# PROCESS: Return Command Name of Specified App
+def get_specified_app(lowercase_text, apps_dict):
+    for app_friendly_lower, command_name in apps_dict.items():
+        if app_friendly_lower in lowercase_text:
+            return command_name
+    return None
 
-            except webbrowser.Error:
-                response(f"{browser_friendly_lower} is currently not available.")
+# PROCESS: Open the Specified Browser
+def open_browser(specified_browser):
+    try:
+        register_browser()
+        response(f"I'm opening {specified_browser} for you...")
+        
+        browser = webbrowser.get(specified_browser)
+        browser.open('https://www.google.com')
 
-# PROCESS: close the specified browser
-def close_browser(entities, doc, browsers_dict):
-    lowercase_text = doc.text.lower()
-    for browser_friendly_lower, command_name in browsers_dict.items():
-        if browser_friendly_lower in lowercase_text:
-            specified_browser = command_name
+    except webbrowser.Error:
+        response(f"{specified_browser} is currently not available.")
 
-            try:
-                response(f"Closing {specified_browser}...")
-                subprocess.run(["taskkill", "/im", f"{specified_browser}.exe", "/f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                break
+# PROCESS: Close the Specified Browser
+def close_browser(specified_browser):
+    try:
+        response(f"Closing {specified_browser}...")
+        subprocess.run(["taskkill", "/im", f"{specified_browser}.exe", "/f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-            except webbrowser.Error:
-                response(f"{browser_friendly_lower} is currently not available.")
+    except webbrowser.Error:
+        response(f"{specified_browser} is currently not available.")
 
-# MAIN: Exit conversation
+# PROCESS: Open the Specified Application
+def open_app(specified_app):
+    try:
+        subprocess.Popen(f"{specified_app}.exe")
+
+    except Exception as e:
+        response(f"I apologize, but it seems that I am unable to open {specified_app}...")
+
+# PROCESS: Close the Specified Application
+def close_app(specified_app):
+    try:
+        response(f"Closing {specified_app}...")
+        subprocess.run(["taskkill", "/im", f"{specified_app}.exe", "/f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    except Exception as e:
+        response(f"I apologize, but it seems that I am unable to close {specified_app}...")
+
+# PROCESS: Load Browser Names
+def load_browser_entities(entities):
+    browsers_dict = entities.get("browsers", {})
+    browsers_dict = {browser.lower(): command for browser, command in browsers_dict.items()}
+    return browsers_dict
+
+# PROCESS: Load Application Names
+def load_app_entities(entities):
+    apps_dict = entities.get("applications", {})
+    apps_dict = {browser.lower(): command for browser, command in apps_dict.items()}
+    return apps_dict
+
+# PROCESS: Open Action Handler
+def handle_open(lowercase_text):
+    entities = load_entities()
+    browsers_dict = load_browser_entities(entities)
+    apps_dict = load_app_entities(entities)
+    
+    specified_browser = get_specified_browser(lowercase_text, browsers_dict)
+    specified_app = get_specified_app(lowercase_text, apps_dict)
+
+    if specified_browser:
+        open_browser(specified_browser)
+    elif specified_app:
+        open_app(specified_app)
+    else:
+        response("Sorry, I don't recognize which app or browser to open.")
+
+# PROCESS: Close Action Handler
+def handle_close(lowercase_text):
+    entities = load_entities()
+    browsers_dict = load_browser_entities(entities)
+    apps_dict = load_app_entities(entities)
+
+    specified_browser = get_specified_browser(lowercase_text, browsers_dict)
+    specified_app = get_specified_app(lowercase_text, apps_dict)
+
+    if specified_browser:
+        close_browser(specified_browser)
+    elif specified_app:
+        close_app(specified_app)
+    else:
+        response("Sorry, I don't recognize which app or browser to close.")
+
+    specified_app = get_specified_app(lowercase_text, apps_dict)
+    if specified_app:
+        close_app(specified_app)
+
+# MAIN: Exit Conversation
 def shutdown_AUTOMA():
     response("See you!")
+    delay(1)
     exit(0)
 
-# MAIN: Start chatbot conversation
+# MAIN: Start Chatbot Conversation
 def initiate_AUTOMA():
     clear_screen()
 
-    # 1. Data Preparation
-    entities, browsers_dict = prepare_data()
+    pygame.mixer.init()
+    pygame.mixer.music.load("assets/background_SFX.mp3")
+    pygame.mixer.music.set_volume(0.1)
+    pygame.mixer.music.play()
+
     # 2. Display TUI Header
     display_header()
     # 3. Welcome User
@@ -147,21 +227,23 @@ def initiate_AUTOMA():
     # 4. Prompt User Response via Voice Recognition
     # user_response = listen_for_response()
     
-    # user_response = "Can you open Google Chrome for me?"
+    user_response = "Can you open Google Chrome for me?"
     # user_response = "Please close Google Chrome."
-    user_response = "Goodbye AUTOMA"
+    # user_response = "Can you open notepad for me please?"
+    # user_response = "Goodbye AUTOMA"
 
     # 5. Process User Response as Prompt
     doc = process_prompt(user_response)
 
-    text = doc.text.lower()
+    lowercase_text = doc.text.lower()
 
+    
     # 6. Initiate Task based on Prompt
-    if "open" in text:
-        open_browser(entities, doc, browsers_dict)
-    elif "close" in text:
-        close_browser(entities, doc, browsers_dict)
-    elif "bye" in text:
+    if "open" in lowercase_text:
+        handle_open(lowercase_text)
+    elif "close" in lowercase_text:
+        handle_close(lowercase_text)
+    elif "bye" in lowercase_text:
         shutdown_AUTOMA()
 
 ''' MAIN '''
